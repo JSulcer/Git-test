e6aff07c09017ec377a58b4b0a72b0c27c5dbc05
Jon Sulcer <jonasulc@ut.utm.edu>
2017-11-30 14:14:12 -0600

    updating main.cpp to include sum function.

--- main.cpp
+++ main.cpp
@@ -6,8 +6,22 @@
 #include <iostream>
 using namespace std;
 
+int sum(int n);
+
 int main(){
   cout << "Hello World!" << endl;
 
+  cout << "Enter the number n: ";
+  int n;
+  cin >> n;
+
+  cout << "Returning sum of numbers 1 to n: " << sum(n) << endl;
   return 0;
+}
+
+int sum(int n){
+  int total = 0;
+  for(int i = 1; i <= n; i++)
+    total += i;
+  return total;
 }
